6a20ef4f4411490b7232530e20911f53c477f9e8
samdinocardozo <samdinocardozo@gmail.com>
2025-08-04 16:13:18 +0530

    updated notification_type controller

--- controllers/notification_type.controller.js
+++ controllers/notification_type.controller.js
@@ -1,54 +1,66 @@
-import NotificationTypeModel from '../models/notification_type.model.js';
+import NotificationTypeModel from '../models/notification_type.model.js'
 import {v4 as uuidv4} from 'uuid';
-// All done
-class NotificationTypeController{
-    async createType(req,res){
-        var {type,carrier}=req.body;
-        type=await type.toLowerCase();
-        carrier=await carrier.toLowerCase();
-        const type_id=uuidv4();
-        try{
-            const result1=await NotificationTypeModel.findByTypeCarrier({type,carrier});
-            if(result1.length>0){
-                res.status(200).json({message:"already saved"});
-            }
-            const result=await NotificationTypeModel.create({type_id,type,carrier});
-            console.log(result);
-            res.status(200).json({message:"successfully added"});
 
+class NotificationTypeController {
+
+    async createType(req,res){
+        var {type, carrier}=req.body;
+        type=type.toLowerCase();
+        carrier= carrier.toLowerCase();
+        const type_id=uuidv4();
+
+        if(!type || !type_id || !carrier) {
+            return res.status(400).json({message: 'fields not specified'});
         }
-        catch(err){
-            res.status(500).send("error").json({message:err});
-        }
+
+    try{
+        const result1=await NotificationTypeModel.findByTypeCarrier({type,carrier});
+            if(result1.length>0){
+                return res.status(200).json({message:"already saved"});
+            }
+
+        const result=await NotificationTypeModel.create({type_id,type,carrier});
+        res.status(201).json({message:'Notification Type added successfully', data: result[0]});
+
+    } catch(err){
+        console.error(err);
+        res.status(500).json({message:'Internal server error'});
+    }
     }
 
     async findType(req,res){
-        const {type,carrier}=req.body;
-        const type_id=uuidv4();
         try{
+            const {type,carrier}=req.body;
             const result=await NotificationTypeModel.findByTypeCarrier({type,carrier});
-            console.log(result);
-            res.status(200).json({result:result});
 
+            if(result.length>0){   
+                res.status(200).json({data: result});
+            } else{
+                res.status(200).json({message: 'Notification types not found.'});
+            }
+
+        } catch(err){
+            console.error(err);
+            res.status(500).json({message:'Internal server error'});
         }
-        catch(err){
-            res.status(500).send("error").json({message:err});
-        }
+
     }
 
-    async find(req,res){
+
+
+  async find(req,res){
         // const {type,carrier}=req.body;
         // const type_id=uuidv4();
         try{
             const result=await NotificationTypeModel.find();
             console.log(result);
-            res.status(200).json({result:result});
+            res.status(200).json({data:result});
 
         }
         catch(err){
-            res.status(500).send("error").json({message:err});
+            console.error(err);
+            res.status(500).json({message:'Internal server error'});
         }
-    }
 }
-
+}
 export default new NotificationTypeController();
