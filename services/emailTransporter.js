bf83233716b79cb03d95837127c959f69e1b8d76
SBM004 <sahanamayya5618@gmail.com>
2025-08-07 10:23:55 +0530

    sss

---
+++ services/emailTransporter.js
@@ -0,0 +1,14 @@
+import nodemailer from 'nodemailer'
+require('dotenv').config();
+
+const transporter = nodemailer.createTransporter({
+  host: 'smtp.sendgrid.net',
+  port: 587,
+  secure: false,
+  auth: {
+    user: 'apikey',
+    pass: process.env.SENDGRID_API_KEY
+  }
+});
+
+module.exports = transporter;
